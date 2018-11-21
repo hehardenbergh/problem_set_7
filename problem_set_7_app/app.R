552ac7efefc66ce72370af3cd7c0a5b7fc7b9d4e
Dillon Smith <dillonmsmith17@Dillons-MacBook-Pro-2.local>
2018-11-20 23:56:52 -0700

    Shiny App Completed

--- problem_set_7_app/app.R
+++ problem_set_7_app/app.R
@@ -2,14 +2,21 @@ library(shiny)
 library(tidyverse)
 library(knitr)
 library(scales)
+library(shinyWidgets)
+library(stargazer)
+library(haven)
+library(sjlabelled)
+library(rsconnect)
+library(shinythemes)
+library(plotly)
 
 shiny_data <- read_rds("final_shiny_data.rds")
-shiny_states <- read_rds("shiny_states.rds")
+
 # Define UI for random distribution app ----
 ui <- fluidPage(
   
   # App title ----
-  titlePanel("2018 Midterm Election Results vs. Forecastss"),
+  titlePanel("2018 Midterm Election Results vs. Forecasts"),
   
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
@@ -20,28 +27,46 @@ ui <- fluidPage(
       # Input: drop-down menu to display observations  ----
       selectInput("x",
                   "X-axis:",
-                   choices = c(`Percent Female` = "pct_female", 
-                              `Percent White` = "pct_white", 
-                              `Percent Likely to Vote` = "pct_likely", 
-                              `Percent with a College Degree` = "pct_college", 
-                              `Percent Undecided Voters` = "pct_und", 
-                              `Percent Millenials` = "pct_mil")),
+                   choices = c("Percent Female" = "pct_female", 
+                               "Percent White" = "pct_white", 
+                               "Percent Likely to Vote" = "pct_likely", 
+                               "Percent with a College Degree" = "pct_college", 
+                               "Percent Undecided Voters" = "pct_und", 
+                               "Percent Millenials" = "pct_mil")),
       selectInput("y",
                   "Y-axis:",
-                  choices = c(`Predicted Democratic Advantage` = "poll_dem_advantage",
-                              `Actual Democratic Advantage` = "actual_dem_advantage"),
-                  multiple = TRUE),
-      selectInput("state",
+                  choices = c("Predicted Democratic Advantage" = "poll_dem_advantage",
+                              "Actual Democratic Advantage" = "actual_dem_advantage")
+                  ),
+      pickerInput("state",
                   "State(s)",
-                  choices = names(shiny_states)),
+                  choices = c("Arizona" = "AZ",
+                              "California" = "CA",
+                              "Colorado" = "CO",
+                              "Florida" = "FL",
+                              "Georgia" = "GA",
+                              "Iowa" = "IA",
+                              "Illinois" = "IL",
+                              "Kansas" = "KS",
+                              "Kentucky" = "KY",
+                              "Maine" = "ME",
+                              "Michigan" = "MI",
+                              "Minnesota" = "MN", 
+                              "North Carolina" = "NC",
+                              "New Jersey" = "NJ",
+                              "New Mexico" = "NM",
+                              "New York" = "NY",
+                              "Ohio" = "OH",
+                              "Pennsylvania" = "PA",
+                              "Texas" = "TX",
+                              "Utah" = "UT",
+                              "Virginia" = "VA",
+                              "Washington" = "WA",
+                              "West Virginia" = "WV"),
+                  options = list(`actions-box` = TRUE),
+                  multiple = TRUE),
       
-      checkboxInput("line", label = "Add linear model"),
-      htmlOutput("see_table"),
-      htmlOutput("regression_table"),
-      
-      
-      # br() element to introduce extra vertical spacing ----
-    #  br(),
+      checkboxInput("line", label = "Add linear model")
   
     ),
     
@@ -51,8 +76,8 @@ ui <- fluidPage(
       # Output: Tabset w/ plot, summary, and table ----
       tabsetPanel(type = "tabs",
                   tabPanel("Plot", plotOutput("plot")),
-                  tabPanel("Polled Democratic Advantage", verbatimTextOutput("summary")),
-                  tabPanel("Actual Democratic Advantage", tableOutput("table"))
+                  tabPanel("Polled Democratic Advantage", htmlOutput("polled")),
+                  tabPanel("Actual Democratic Advantage", htmlOutput("actual"))
       )
     )
   )
@@ -67,54 +92,54 @@ server <- function(input, output) {
   # both tracked, and all expressions are called in the sequence
   # implied by the dependency graph.
   output$plot <- renderPlot({
+    
+    plot_data <- shiny_data %>% filter(state %in% input$state)
+    
     if(input$line == TRUE) {
-      shiny_data %>%
-        filter(position %in% input$type) %>%
-        filter(state %in% state.abb[match(input$state,state.name)]) %>%
-        ggplot(aes(x = predicted_rep_advantage, y = actual_rep_advantage)) +
+        ggplot(plot_data, aes_string(x = input$x, y = input$y)) +
         geom_point() +
         geom_smooth(method="lm", se=FALSE) +
-        labs(x = "Predicted",
-             y = "Actual",
-             title = "Predicted vs. Actual Republican Advantage in U.S. 2018 Midterm Election",
-            subtitle = "Calculated by dividing the difference between Republican and Democrat votes by total votes cast")
-    }
-    else{
-      shiny_data %>%
-        filter(position %in% input$type) %>% 
-        filter(state %in% state.abb[match(input$state,state.name)]) %>%
-        ggplot(aes(x = predicted_rep_advantage, y = actual_rep_advantage)) +
-        geom_point() +
-        labs(x = "Predicted",
-             y = "Actual",
+        labs(x = "Selected X-Value (Percentage)",
+             y = "Selected Y-Value",
              title = "Predicted vs. Actual Republican Advantage in U.S. 2018 Midterm Election",
              subtitle = "Calculated by dividing the difference between Republican and Democrat votes by total votes cast")
     }
-  })
+    else{
+      ggplot(plot_data, aes_string(x = input$x, y = input$y)) +
+        geom_point() +
+        labs(x = "Selected X-Value (Percentage)",
+             y = "Selected Y-Value",
+             title = "Predicted vs. Actual Republican Advantage in U.S. 2018 Midterm Election",
+             subtitle = "Calculated by dividing the difference between Republican and Democrat votes by total votes cast")
+    }
+    })
    
-  # # display regression table
-  # output$regression_table <- renderUI({
-  #   filteredData <- reactive ({
-  #     df <- app_data[app_data$state %in% input$state,]
-  #   })
-  #   
-  #   
-  # # Generate a summary of the data ----
-  # output$summary <- renderPrint({
-  #   
-  #   shiny_data %>% 
-  #     select(input$data) %>% 
-  #     summary()
-  #   
-  # })
+  # display regression table
+  output$polled <- renderUI({
+     
+    HTML(
+      stargazer(lm(data = shiny_data, poll_dem_advantage ~ pct_female + pct_white + pct_likely + pct_college + pct_und + pct_mil), 
+                type = "html",
+                column.labels = "Polled Democratic Advantage",
+                covariate.labels = c("Percent Female", "Percent White", "Percent Likely to Vote", "Percent with College Degree +", "Percent Undecided", "Percent Millenial"),
+                notes = "Covariates calculated using Upshot data: Percent Female calculated by dividing number of female respondents in each district by total respondents, Percent White calculated by dividing number of white respondents in each district by total respondents, Percent Likely to Vote calculated by dividing number of respondents in \"Already voted\", \"Almost certain\", and \"Very likely\" by total respondents, Percent with College Degree or More	calculated by dividing number of respondents with a 4-year college degree or more by total respondents, Percent Undecided calculated by dividing number of respondents who were Undecided by total respondents, and Percent Millenial calculated by dividing number of respondents below 34 by total respondents.",
+                notes.align = "c")
+    )
+  })
   
-  # # Generate an HTML table view of the data ----
-  # output$table <- renderTable({
-  #   
-  #   shiny_data %>% 
-  #      
-  #   
-  # })
+  
+  # Generate a summary of the data ----
+  output$actual <- renderUI({
+  
+    HTML(
+      stargazer(lm(data = shiny_data, actual_dem_advantage ~ pct_female + pct_white + pct_likely + pct_college + pct_und + pct_mil), 
+                type = "html",
+                column.labels = "Actual Democratic Advantage",
+                covariate.labels = c("Percent Female", "Percent White", "Percent Likely to Vote", "Percent with College Degree +", "Percent Undecided", "Percent Millenial"),
+                notes = "Covariates calculated using Upshot data: Percent Female calculated by dividing number of female respondents in each district by total respondents, Percent White calculated by dividing number of white respondents in each district by total respondents, Percent Likely to Vote calculated by dividing number of respondents in \"Already voted\", \"Almost certain\", and \"Very likely\" by total respondents, Percent with College Degree or More	calculated by dividing number of respondents with a 4-year college degree or more by total respondents, Percent Undecided calculated by dividing number of respondents who were Undecided by total respondents, and Percent Millenial calculated by dividing number of respondents below 34 by total respondents.",
+                notes.align = "c")
+    )
+   })
   
 }
 
